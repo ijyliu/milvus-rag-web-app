d0fbacc42de5ef2f035070623521bc37e9cb477d
Isaac Liu <ijyliu@uchicago.edu>
2025-05-11 12:14:07 -0400

    recongifure deployment ports and regions and successfully test chat container

--- Ollama Gemma Container/gcr_deployment.ps1
+++ Ollama Gemma Container/gcr_deployment.ps1
@@ -1,1 +1,1 @@
-gcloud run deploy ollama-gemma --image=gcr.io/milvus-rag-web-app/ollama-gemma:latest --region=us-central1 --allow-unauthenticated --memory=8G --cpu=2 --max-instances=2
+gcloud run deploy ollama-gemma --image gcr.io/milvus-rag-web-app/ollama-gemma:latest --region us-central1 --allow-unauthenticated --port 3000 --memory 8G --cpu 2 --max-instances 2
