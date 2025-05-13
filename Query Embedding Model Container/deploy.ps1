e271b231dd8edd6b968f11f97e889c3059e01da7
Isaac Liu <ijyliu@uchicago.edu>
2025-05-12 21:32:03 -0400

    new deployment scripts but still have probs

--- Query Embedding Model Container/deploy.ps1
+++ Query Embedding Model Container/deploy.ps1
@@ -1,3 +1,3 @@
-docker build -t gcr.io/milvus-rag-web-app/query-embedding-model:latest .
-docker push gcr.io/milvus-rag-web-app/query-embedding-model:latest
-gcloud run deploy query-embedding-model --image gcr.io/milvus-rag-web-app/query-embedding-model:latest --region us-central1 --no-allow-unauthenticated --port 5000 --memory 8G --cpu 2 --max-instances 2
+docker build -t us-central1-docker.pkg.dev/milvus-rag-web-app/milvus-rag-web-app/query-embedding-model:latest .
+docker push us-central1-docker.pkg.dev/milvus-rag-web-app/milvus-rag-web-app/query-embedding-model:latest
+gcloud run deploy query-embedding-model --image us-central1-docker.pkg.dev/milvus-rag-web-app/milvus-rag-web-app/query-embedding-model:latest --region us-central1 --no-allow-unauthenticated --port 5000 --memory 8G --cpu 2 --max-instances 2
--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,3 @@
-docker build -t gcr.io/milvus-rag-web-app/milvus-rag-web-app:latest .
-docker push gcr.io/milvus-rag-web-app/milvus-rag-web-app:latest
-gcloud run deploy milvus-rag-web-app --image=gcr.io/milvus-rag-web-app/milvus-rag-web-app:latest --region=us-central1 --allow-unauthenticated --max-instances=2
+docker build -t us-central1-docker.pkg.dev/milvus-rag-web-app/milvus-rag-web-app/milvus-rag-web-app:latest .
+docker push us-central1-docker.pkg.dev/milvus-rag-web-app/milvus-rag-web-app/milvus-rag-web-app:latest
+gcloud run deploy milvus-rag-web-app --image=us-central1-docker.pkg.dev/milvus-rag-web-app/milvus-rag-web-app/milvus-rag-web-app:latest --region=us-central1 --allow-unauthenticated --max-instances=2
