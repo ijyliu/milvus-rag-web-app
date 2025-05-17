625adbe074a63aaf5e75e2b491f282441c19f28e
Isaac Liu <ijyliu@gmail.com>
2025-05-17 16:38:20 -0400

    reconfigure app to use localhost

---
+++ Query Embedding Model Container/build_and_push.ps1
@@ -0,0 +1,2 @@
+docker build -t us-central1-docker.pkg.dev/milvus-rag-web-app/milvus-rag-web-app/query-embedding-model:latest .
+docker push us-central1-docker.pkg.dev/milvus-rag-web-app/milvus-rag-web-app/query-embedding-model:latest
